8351f7863a95009e37c4eb8ec2b31042156a3789
benpkxd <113038163+benpkxd@users.noreply.github.com>
2022-12-15 17:28:19 +0800

    Add files via upload
    Learn about basic bash command

---
+++ Intro to Bash command.sh
@@ -0,0 +1,29 @@
+#list File
+ls
+echo Hello World
+#create file and add content
+echo Hi > Text1.txt
+#overwrite content
+echo Hi2 > Text1.txt
+#append content (Not to overwrite)
+echo Hi3 >> Text1.txt
+# concatenate
+cat Text1.txt
+# remove file
+rm Text1.txt
+
+#create new file and add content
+echo Ben > Text2.txt
+echo Ben2 >> Text2.txt
+echo Ben3 >> Text2.txt
+
+# create folder
+mkdir Folder1
+
+#remove folder
+rmdir Folder1
+
+#create folder
+mkdir Folder2
+#remove folder
+rmdir Folder2
